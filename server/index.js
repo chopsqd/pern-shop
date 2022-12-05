caf5a4ee15d9467e6ac16bd572df12f8721c748a
Michael Chop <007micle007@gmail.com>
2022-12-05 16:33:18 +0300

    Added models to the project

--- server/index.js
+++ server/index.js
@@ -1,9 +1,18 @@
 require('dotenv').config()
 const express = require('express')
 const sequelize = require('./db')
+const cors = require('cors')
+const model = require('./models/models')
 
-const app = express()
 const PORT = process.env.PORT || 5000
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('Bruhhhh')
+})
 
 const start = async () => {
     try {
---
+++ server/models/models.js
@@ -0,0 +1,90 @@
+const sequelize = require('../db')
+const {DataTypes} = require('sequelize')
+
+const User = sequelize.define('user', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    email: { type: DataTypes.STRING, unique: true },
+    password: { type: DataTypes.STRING },
+    role: { type: DataTypes.STRING, defaultValue: "USER" }
+})
+
+const Basket = sequelize.define('basket', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true }
+})
+
+const BasketDevice = sequelize.define('basket_device', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true }
+})
+
+const Device = sequelize.define('device', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    name: { type: DataTypes.STRING, unique: true,  allowNull: false },
+    price: { type: DataTypes.INTEGER, allowNull: false },
+    rating: { type: DataTypes.INTEGER, defaultValue: 0 },
+    img: { type: DataTypes.STRING, allowNull: false }
+})
+
+const Type = sequelize.define('type', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    name: { type: DataTypes.STRING, unique: true,  allowNull: false }
+})
+
+const Brand = sequelize.define('brand', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    name: { type: DataTypes.STRING, unique: true,  allowNull: false }
+})
+
+const Rating = sequelize.define('rating', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    rate: { type: DataTypes.INTEGER, allowNull: false }
+})
+
+const DeviceInfo = sequelize.define('device_info', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    title: { type: DataTypes.STRING, allowNull: false },
+    description: { type: DataTypes.STRING, allowNull: false }
+})
+
+// Связующая таблица(модель)
+const TypeBrand = sequelize.define('type_brand', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true }
+})
+
+// Связываем таблицы между собой (указываем типы связей) //
+
+// Одна запись Device содержит много записей DeviceInfo
+Device.hasMany(DeviceInfo)
+// DeviceInfo принадлежит Device
+DeviceInfo.belongsTo(Device)
+
+User.hasOne(Basket)
+Basket.belongsTo(User)
+
+User.hasMany(Rating)
+Rating.belongsTo(User)
+
+Basket.hasMany(BasketDevice)
+BasketDevice.belongsTo(Basket)
+
+Type.hasMany(Device)
+Device.belongsTo(Type)
+
+Brand.hasMany(Device)
+Device.belongsTo(Brand)
+
+Device.hasMany(Rating)
+Rating.belongsTo(Device)
+
+Device.hasMany(BasketDevice)
+BasketDevice.belongsTo(Device)
+
+// ВАЖНО!
+// При связи Многие ко Многим создается промежуточная таблица,
+// которая хранит инфо о том, какой Brand принадлежит какому Type
+// и какой Type связан с каким Brand, поэтому указываем { through: связующая таблица(модель) }
+Type.belongsToMany(Brand, {through: TypeBrand})
+Brand.belongsToMany(Type, {through: TypeBrand})
+
+module.exports = {
+    User, Basket, BasketDevice, Device, Type, Brand, Rating, TypeBrand, DeviceInfo
+}
