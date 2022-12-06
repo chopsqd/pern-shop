d5c95604665ede871e0d73f3003e5b38db103383
Michael Chop <007micle007@gmail.com>
2022-12-06 09:43:56 +0300

    Added routing to the project

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import {BrowserRouter} from "react-router-dom";
+import AppRouter from "./components/AppRouter";
 
 const App = () => {
     return (
-        <div>
-            Work
-        </div>
+        <BrowserRouter>
+            <AppRouter />
+        </BrowserRouter>
     );
 };
 
---
+++ client/src/components/AppRouter.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {Routes, Route, Navigate} from 'react-router-dom'
+import {authRoutes, publicRoutes} from "../routes";
+import {SHOP_ROUTE} from "../utils/consts";
+
+const AppRouter = () => {
+    const isAuth = false
+
+    return (
+        <Routes>
+            {isAuth && authRoutes.map(({path, Component}) =>
+                <Route key={path} path={path} element={<Component/>} exact/>
+            )}
+            {publicRoutes.map(({path, Component}) =>
+                <Route key={path} path={path} element={<Component/>} exact/>
+            )}
+            <Route path='*' element={<Navigate to={SHOP_ROUTE}/>} />
+        </Routes>
+    );
+};
+
+export default AppRouter;
